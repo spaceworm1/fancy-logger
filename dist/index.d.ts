6c4f1144ca53b83d8ec733ecb3661dfaeeae4255
Irakli Kverenchkhiladze <irakli_kverenchkhiladze@epam.com>
2022-12-19 22:03:32 +0400

    added d.ts

---
+++ dist/index.d.ts
@@ -0,0 +1,9 @@
+export default class FancyLogger {
+    static green(text: string): void;
+    static red(text: string): void;
+    static blue(text: string): void;
+    static yellow(text: string): void;
+    static cyan(text: string): void;
+    static log(text: string): void;
+}
+//# sourceMappingURL=index.d.ts.map
